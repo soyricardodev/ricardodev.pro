e1cf1eb57dcc9be29c3ddac8e9c4d553f48cebb3
Ricardo <soyricardodev@proton.me>
2024-01-26 17:15:44 +0000

    add prettier and constants

---
+++ src/constants/seo.ts
@@ -0,0 +1,4 @@
+export default {
+  title: 'Porfolio de Ricardo Castro: Desarrollador Web Full Stack',
+  description: 'Contrata a Ricardo para crear tu aplicación web o móvil. Desarrollador Web Full Stack con 3 years de experiencia. Especializado en crear aplicaciones accesibles.'
+}
