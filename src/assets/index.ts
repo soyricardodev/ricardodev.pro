bc939f08a927d821bab3d275a1f5b79950b53920
Ricardo <soyricardodev@proton.me>
2024-02-04 05:57:06 +0000

    add images for support astro images api

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import projectDolary from "./project-dolary.webp";
+import projectEnfoque from "./project-enfoque.webp";
+import projectLtaxis from "./project-ltaxis.webp";
+import projectMedusa from "./project-medusa.webp";
+
+export { projectDolary, projectEnfoque, projectLtaxis, projectMedusa };
