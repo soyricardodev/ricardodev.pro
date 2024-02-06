6588f3637df6385a8f6069f8c919ef77e51e5318
Ricardo <soyricardodev@proton.me>
2024-02-06 20:22:34 +0000

    finish

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,6 @@
 import projectDolary from "./project-dolary.webp";
 import projectEnfoque from "./project-enfoque.webp";
 import projectLtaxis from "./project-ltaxis.webp";
-import projectMedusa from "./project-medusa.webp";
-
-export { projectDolary, projectEnfoque, projectLtaxis, projectMedusa };
+import projectMedusa from "./project-ally.webp";
+import me from "./me.webp";
+export { projectDolary, projectEnfoque, projectLtaxis, projectMedusa, me };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,6 +15,19 @@ export default {
       fontFamily: {
         sans: ["Onest Variable", ...fontFamily.sans],
       },
+      // animation: {
+      //   "background-shine": "background-shine 2s linear infinite",
+      // },
+      // keyframes: {
+      //   "background-shine": {
+      //     from: {
+      //       backgroundPosition: "0 0",
+      //     },
+      //     to: {
+      //       backgroundPosition: "-200% 0",
+      //     },
+      //   },
+      // },
     },
   },
   plugins: [],
