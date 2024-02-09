35f963467651559317fed114a68386de7fd6481e
Ricardo <soyricardodev@proton.me>
2024-02-09 00:04:25 +0000

    fix nav error and add logo

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,4 +3,20 @@ import projectEnfoque from "./project-enfoque.webp";
 import projectLtaxis from "./project-ltaxis.webp";
 import projectMedusa from "./project-ally.webp";
 import me from "./me.webp";
-export { projectDolary, projectEnfoque, projectLtaxis, projectMedusa, me };
+
+import logobase32x32 from "./logo-base-32x32.png";
+import logobase256x256 from "./logo-base-256x256.png";
+import logowhite32x32 from "./logo-white-32x32.png";
+import logowhite256x256 from "./logo-white-256x256.png";
+
+export {
+  projectDolary,
+  projectEnfoque,
+  projectLtaxis,
+  projectMedusa,
+  me,
+  logobase32x32,
+  logobase256x256,
+  logowhite32x32,
+  logowhite256x256,
+};
