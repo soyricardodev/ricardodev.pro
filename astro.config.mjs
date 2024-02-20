f98fe8aa6c6785ff4a42a03f9e7dcac81cb92141
Ricardo <soyricardodev@proton.me>
2024-02-20 14:21:34 +0000

    remove white mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://soyricardo.vercel.app/",
+  site: "https://ricardodev.pro",
   integrations: [
     tailwind({
       // applyBaseStyles: false,
